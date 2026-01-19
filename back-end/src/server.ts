58a25ad83f44c24cd5b54f5d00886035689041cd
BulusHamnu <hamnubulus@gmail.com>
2026-01-19 20:19:53 +0100

    Feat: added helmet for security headers

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -6,7 +6,6 @@ import morgan from "morgan";
 import type { Express, Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import logger from "./utils/logger.js";
-import AppError from "./errors/appError.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users/users.routes.js";
@@ -15,9 +14,17 @@ import trackRoutes from "./routes/tracks/track.routes.js";
 import publicApis from "./routes/public.apis.routes.js";
 import newsLetterRoutes from "./routes/newsletter.routes.js";
 import checkoutRoutes from "./routes/checkout.routes.js";
+import helmet from "helmet";
 
-// initiate server
+// Initiate server
 const app: Express = express();
+
+// Middlewares
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 app.use(
@@ -31,21 +38,14 @@ app.use(
 );
 app.use(cookieParser());
 
-// routes
-app.get(
-  "/",
-  (
-    req: Request<{}, {}, {}, {}>,
-    res: Response<{}>,
-    next: NextFunction
-  ): void => {
-    try {
-      res.send("<h1>Hello World</h1>");
-    } catch (error: unknown) {
-      next(error);
-    }
+// Routes
+app.get("/", (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    res.send("<h1>Hello World</h1>");
+  } catch (error: unknown) {
+    next(error);
   }
-);
+});
 app.use("/api/v1/contact-me", publicApis);
 app.use("/api/v1/news-letter", newsLetterRoutes);
 app.use("/api/v1/auth", authRoutes);
