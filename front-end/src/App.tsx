242addcef59af1631a440cea31d2a75056c30851
BulusHamnu <hamnubulus@gmail.com>
2026-04-30 15:33:14 +0100

    Initiated frontend with Vite+React, Typescript and Tailwind

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <main>
+        <h1>Siwel Beats App</h1>
+        <p>Hello, Peterrr!!!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
