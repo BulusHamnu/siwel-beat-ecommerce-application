f3554b5ec35cc8fc0bbbbc8af2247c4ba68947b9
BulusHamnu <hamnubulus@gmail.com>
2026-06-22 13:18:50 +0100

    feat: add notification panel on header.

--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -8,12 +8,16 @@ import {
   LogOut,
   ShoppingBag,
   CreditCard,
+  XIcon,
+  RotateCwIcon,
+  ArrowUpRight,
 } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { motion } from "motion/react";
 import toast from "react-hot-toast";
+import formatTimeAgo from "../helpers/helpers";
 
 type To =
   | string
@@ -22,6 +26,45 @@ type To =
       search?: string;
     };
 
+function LinkItem({
+  text,
+  to,
+  type,
+  setIsOpen,
+}: {
+  text: string;
+  to: To;
+  type?: string;
+  setIsOpen: (value: boolean) => void;
+}) {
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const handleLiClick = () => {
+    linkRef.current?.click();
+  };
+
+  return (
+    <li
+      onClick={() => handleLiClick()}
+      className="cursor-pointer p-3 py-5 text-center max-md:text-lg md:p-1 w-full hover:bg-[#1c567e] transition-colors duration-300"
+    >
+      {type === "normal" ? (
+        <a
+          ref={linkRef}
+          onClick={() => setIsOpen(false)}
+          href={typeof to === "string" ? to : "#"}
+        >
+          {text}
+        </a>
+      ) : (
+        <NavLink ref={linkRef} onClick={() => setIsOpen(false)} to={to}>
+          {text}
+        </NavLink>
+      )}
+    </li>
+  );
+}
+
+/* Profile Menu */
 function MenuItem({ to, children }: { to: To; children: any }) {
   return (
     <li className="profile-menu-item">
@@ -119,41 +162,127 @@ function ProfileMenu({
   );
 }
 
-function LinkItem({
-  text,
-  to,
-  type,
+/* Notification Panel */
+function NotificationPanel({
+  isOpen,
   setIsOpen,
 }: {
-  text: string;
-  to: To;
-  type?: string;
+  isOpen: boolean;
   setIsOpen: (value: boolean) => void;
 }) {
-  const linkRef = useRef<HTMLAnchorElement>(null);
-  const handleLiClick = () => {
-    linkRef.current?.click();
-  };
+  // const { status, action, logout, isLoading, isAutheticated } = useAuth();
+  const panelRef = useRef<HTMLDivElement | null>(null);
+
+  interface Notification {
+    id: string;
+    type: string;
+    message: string;
+    read: boolean;
+    resourceId: string;
+    entityId: string;
+    date: string | Date;
+  }
+
+  const [notifications] = useState<Notification[] | null>(null);
+
+  useEffect(() => {
+    const handleBodyClick = (e: any) => {
+      if (panelRef.current && panelRef.current.contains(e.target)) {
+        return;
+      }
+
+      if (isOpen) setIsOpen(false);
+    };
+
+    document.body.addEventListener("click", handleBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", handleBodyClick);
+    };
+  }, [isOpen, setIsOpen]);
 
   return (
-    <li
-      onClick={() => handleLiClick()}
-      className="cursor-pointer p-3 py-5 text-center max-md:text-lg md:p-1 w-full hover:bg-[#1c567e] transition-colors duration-300"
+    <motion.div
+      ref={panelRef}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      id="notification-panel"
+      className="bg-[rgba(2,21,38,1)] absolute border-2 border-white rounded-md p-1.5 h-fit w-[320px] min-[500px]:w-100  right-[-350%] min-[360px]:right-[-310%] top-[115%] min-[690px]:right-[50%] "
     >
-      {type === "normal" ? (
-        <a
-          ref={linkRef}
-          onClick={() => setIsOpen(false)}
-          href={typeof to === "string" ? to : "#"}
-        >
-          {text}
-        </a>
+      <div
+        id="notification-panel-header"
+        className="flex flex-row flex-nowrap justify-between items-center py-0.5 px-1.5 mt-1"
+      >
+        <h2 style={{ margin: 0 }} className="">
+          Notifications
+        </h2>
+        <div className="flex flex-row flex-nowrap items-center gap-4">
+          {notifications && notifications.length > 0 && (
+            <button className="button-primary text-xs">Mark All As Read</button>
+          )}
+          <span className="cursor-pointer p-1 rounded hover:bg-[#1c567e] transition-colors duration-300">
+            <RotateCwIcon size={25} />
+          </span>
+          <span
+            onClick={() => setIsOpen(false)}
+            className="cursor-pointer p-1 rounded hover:bg-[#1c567e] transition-colors duration-300"
+          >
+            <XIcon size={25} />
+          </span>
+        </div>
+      </div>
+      {notifications && notifications.length > 0 ? (
+        <ul className="mt-5 max-h-112.5 overflow-y-auto scrollbar-none [&::-webkit-scrollbar]:hidden">
+          {notifications.map((notification) => (
+            <li
+              key={notification.id}
+              className={`${notification.read ? "bg-[rgba(6,43,88,0.3)]" : "bg-[rgba(6,43,88,0.6)]"} py-1.5 px-3 border border-gray-600 rounded mb-2`}
+            >
+              <div>
+                <a
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    alert(notification.type);
+                  }}
+                  href="#"
+                  className="flex flex-row justify-between items-center hover:underline"
+                >
+                  <p
+                    style={{ fontFamily: "Inter, sans-serif" }}
+                    className="text-lg whitespace-break-spaces text-left"
+                  >
+                    {notification.message}
+                  </p>
+                  <ArrowUpRight size={20} />
+                </a>
+              </div>
+              <div className="flex flex-row flex-nowrap justify-between items-center mt-3">
+                <span className="text-sm">
+                  {formatTimeAgo(notification.date)}
+                </span>
+                <div className="flex flex-row flex-nowrap gap-2">
+                  <button
+                    onClick={() => alert("Deleted!")}
+                    className="cursor-pointer text-sm py-1 px-2 bg-blue-700 rounded hover:bg-blue-800 transition duration-300 w-22.5"
+                  >
+                    {notification.read ? "Mark Unread" : "Mark Read"}
+                  </button>
+                  <button
+                    onClick={() => alert("Deleted!")}
+                    className="cursor-pointer text-sm py-1 px-2 bg-red-700 rounded hover:bg-red-800 transition duration-300 w-22.5"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       ) : (
-        <NavLink ref={linkRef} onClick={() => setIsOpen(false)} to={to}>
-          {text}
-        </NavLink>
+        <span className="block p-4 text-lg">No Notification Found.</span>
       )}
-    </li>
+    </motion.div>
   );
 }
 
@@ -161,6 +290,7 @@ function LinkItem({
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
   const { isAutheticated } = useAuth();
 
   useEffect(() => {
@@ -250,12 +380,27 @@ function Header() {
           >
             <ShoppingCart size={25} />
           </Link>
-          <span className="cursor-pointer p-1 rounded hover:bg-[#1c567e] transition-colors duration-300">
-            <Bell size={25} />
+          {/* Notification Icon */}
+          <span className="p-1 rounded hover:bg-[#1c567e] transition-colors duration-300 relative z-50">
+            <Bell
+              className="cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                setIsNotificationOpen(!isNotificationOpen);
+              }}
+              size={25}
+            />
+            {isNotificationOpen && (
+              <NotificationPanel
+                isOpen={isNotificationOpen}
+                setIsOpen={setIsNotificationOpen}
+              />
+            )}
           </span>
           {/* Profile Icon */}
-          <span className="cursor-pointer p-1 rounded hover:bg-[#1c567e] transition-colors duration-300 relative z-50">
+          <span className="p-1 rounded hover:bg-[#1c567e] transition-colors duration-300 relative z-50">
             <User
+              className="cursor-pointer"
               onClick={(e) => {
                 e.stopPropagation();
                 setIsMenuOpen(!isMenuOpen);
--- front-end/src/helpers/helpers.ts
+++ front-end/src/helpers/helpers.ts
@@ -1,3 +1,7 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 /* Format currencies*/
 export function formatAmount(cents: number, currency = "USD") {
   const dollars = cents / 100;
@@ -5,4 +9,9 @@ export function formatAmount(cents: number, currency = "USD") {
     style: "currency",
     currency: currency,
   }).format(dollars);
+}
+
+/* Format time ago */
+export default function formatTimeAgo(date: Date | string) {
+  return dayjs(date).fromNow();
 }
